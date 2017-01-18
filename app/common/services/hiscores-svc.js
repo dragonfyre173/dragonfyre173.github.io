b6121ea4f6e0eb888899cca181c1998e0e6d590a
Landon Reams <landon.reams.13@cnu.edu>
2017-01-17 23:00:41 -0500

    Added hiscore parsing
    Should properly parse hiscores data now.

--- app/common/services/hiscores-svc.js
+++ app/common/services/hiscores-svc.js
@@ -13,11 +13,8 @@
         });
     }
 
-    function Hiscores($http) {
-        var _factory = this;
-        var _service = { };
-
-        _factory.headers = [
+    function parseHiscores(yqlResponse) {
+        var headers = [
             "Total", "Attack", "Defence", "Strength",
             "Hitpoints", "Ranged", "Prayer", "Magic",
             "Cooking", "Woodcutting", "Fletching", "Fishing",
@@ -26,13 +23,35 @@
             "Farming", "Runecrafting", "Hunter", "Construction"
         ];
 
+        var data = yqlResponse.results;
+
+        var results = { };
+
+        for(var i = 0; i < headers.length; i++) {
+            var hsEntry = data[i];
+            results[headers[i]] = {
+                rank: hsEntry.col0,
+                level: hsEntry.col1,
+                exp: hsEntry.col2
+            }
+        }
+
+        return results;
+    }
+
+    function Hiscores($http) {
+        var _factory = this;
+        var _service = { };
+
+
+
         _factory.baseUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fservices.runescape.com%2Fm%3Dhiscore_oldschool%2Findex_lite.ws%3Fplayer%3D{0}'&format=json&diagnostics=true&callback=";
 
         _service.getPlayer = function(name) {
             return $http.get(formatString(_factory.baseUrl, "lord_lothric"), {
 
             }).then(function(response){
-                return response.data;
+                return parseHiscores(response.data);
             });
         }
         return _service;
