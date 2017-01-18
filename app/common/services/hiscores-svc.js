71b93e52881763c05c60a0e3ae9f4aa264cad293
Landon Reams <landon.reams.13@cnu.edu>
2017-01-17 22:46:51 -0500

    Switched to YQL
    Runescape API doesn't support CORS, had to switch to YQL to parse.

--- app/common/services/hiscores-svc.js
+++ app/common/services/hiscores-svc.js
@@ -3,21 +3,19 @@
         .module('app')
         .factory('Hiscores', ['$http', Hiscores]);
 
+    function formatString(format) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        return format.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    }
+
     function Hiscores($http) {
         var _factory = this;
         var _service = { };
-
-        if (!String.format) {
-          String.format = function(format) {
-            var args = Array.prototype.slice.call(arguments, 1);
-            return format.replace(/{(\d+)}/g, function(match, number) {
-              return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-              ;
-            });
-          };
-        }
 
         _factory.headers = [
             "Total", "Attack", "Defence", "Strength",
@@ -28,14 +26,14 @@
             "Farming", "Runecrafting", "Hunter", "Construction"
         ];
 
-        _factory.baseUrl = "https://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
+        _factory.baseUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fservices.runescape.com%2Fm%3Dhiscore_oldschool%2Findex_lite.ws%3Fplayer%3D{0}'&format=json&diagnostics=true&callback=yqlParseHiscores";
 
 
         _service.getPlayer = function(name) {
-            return $http.get(_factory.baseUrl+name, {
+            return $http.get(formatString(_factory.baseUrl, "lord_lothric"), {
 
             }).then(function(response){
-                return response.data;
+                return response.data.results;
             });
         }
 //        _service.getPlayer = function(name) {
